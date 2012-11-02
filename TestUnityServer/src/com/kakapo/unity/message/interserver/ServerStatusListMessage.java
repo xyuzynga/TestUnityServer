596d1e026252b3887eefaaa0b932522032e69c18
felix.vincent <felix.vincent@drdindia-021.drd.co.in>
2012-11-02 08:37:46 +0530

    ServerStatusListMessage modified Signed-off-by:felix.vincent <felix.vincent@drdindia-021.drd.co.in>

--- TestUnityServer/src/com/kakapo/unity/message/interserver/ServerStatusListMessage.java
+++ TestUnityServer/src/com/kakapo/unity/message/interserver/ServerStatusListMessage.java
@@ -1,18 +1,25 @@
 package com.kakapo.unity.message.interserver;
 
 import com.kakapo.unity.message.ExtensionStatus;
-import com.kakapo.unity.message.Message;
-import com.kakapo.unity.message.server.*;
+import java.util.Date;
 
-public class ServerStatusListMessage extends Message
+public class ServerStatusListMessage extends ServerMessage
 {
   public static final String COMMAND = "StatusList";
+  private final String group;
   private final ExtensionStatus _status;
 
-  public ServerStatusListMessage(ExtensionStatus status)
+  public ServerStatusListMessage(String group, ExtensionStatus status)
   {
     super("StatusList");
+    this.group = group;
     this._status = status;
   }
 
+  public ServerStatusListMessage(String group, String extension, String status, Date since)
+  {
+    super("StatusList");
+    this.group = group;
+    this._status = new ExtensionStatus(extension, status, since);
+  }
 }
